614b70019e68d2e9b0a2401393c244bae7469c9d
Marcos Baez <mbaezpy@gmail.com>
2015-11-24 14:33:38 +0100

    Initial work on color+

--- data.js
+++ data.js
@@ -5,99 +5,230 @@ var data = [
  day : "Wednesday",
  period : "morning",
  temperature : 29,
+ city : "trento",
  condition: "sunny"
 },   
 {
  day : "Wednesday",
  period : "noon",
  temperature : 32,
+ city : "trento",
  condition: "sunny"
 },   
 {
  day : "Wednesday",
  period : "afternoon",
  temperature : 34,
+ city : "trento",
  condition: "sunny"
 },   
 {
  day : "Wednesday",
  period : "evening",
  temperature : 33,
+ city : "trento",
  condition: "sunny"
 },
+{
+ day : "Wednesday",
+ period : "morning",
+ temperature : 29,
+ city : "rovereto",
+ condition: "sunny"
+},   
+{
+ day : "Wednesday",
+ period : "noon",
+ temperature : 33,
+ city : "rovereto",
+ condition: "sunny"
+},   
+{
+ day : "Wednesday",
+ period : "afternoon",
+ temperature : 35,
+ city : "rovereto",
+ condition: "sunny"
+},   
+{
+ day : "Wednesday",
+ period : "evening",
+ temperature : 33,
+ city : "rovereto",
+ condition: "sunny"
+}, 
+  
 {
  day : "Thursday",
  period : "morning",
  temperature : 27,
+ city : "trento",
  condition: "sunny-cloudy"
 },   
 {
  day : "Thursday",
  period : "noon",
  temperature : 31,
+ city : "trento",
  condition: "sunny-cloudy"
 },   
 {
  day : "Thursday",
  period : "afternoon",
  temperature : 32,
+ city : "trento",
  condition: "sunny-cloudy"
 },   
 {
  day : "Thursday",
  period : "evening",
  temperature : 30,
+ city : "trento",
  condition: "sunny-cloudy"
 },
+{
+ day : "Thursday",
+ period : "morning",
+ temperature : 28,
+ city : "rovereto",
+ condition: "sunny-cloudy"
+},   
+{
+ day : "Thursday",
+ period : "noon",
+ temperature : 33,
+ city : "rovereto",
+ condition: "sunny-cloudy"
+},   
+{
+ day : "Thursday",
+ period : "afternoon",
+ temperature : 34,
+ city : "rovereto",
+ condition: "sunny-cloudy"
+},   
+{
+ day : "Thursday",
+ period : "evening",
+ temperature : 31,
+ city : "rovereto",
+ condition: "sunny-cloudy"
+},   
+  
  
 {
  day : "Friday",
  period : "morning",
  temperature : 28,
+ city : "trento",
  condition : "sunny-rainy"
 },   
 {
  day : "Friday",
  period : "noon",
  temperature : 32,
+ city : "trento",
  condition : "sunny-rainy"
 },   
 {
  day : "Friday",
  period : "afternoon",
  temperature : 33,
+ city : "trento",
  condition : "sunny-rainy"
 },   
 {
  day : "Friday",
  period : "evening",
  temperature : 31,
+ city : "trento",
  condition : "sunny-rainy"
 },
+{
+ day : "Friday",
+ period : "morning",
+ temperature : 29,
+ city : "rovereto",
+ condition : "sunny-rainy"
+},   
+{
+ day : "Friday",
+ period : "noon",
+ temperature : 34,
+ city : "rovereto",
+ condition : "sunny-rainy"
+},   
+{
+ day : "Friday",
+ period : "afternoon",
+ temperature : 35,
+ city : "rovereto",
+ condition : "sunny-rainy"
+},   
+{
+ day : "Friday",
+ period : "evening",
+ temperature : 32,
+ city : "rovereto",
+ condition : "sunny-rainy"
+},   
+  
 
 {
  day : "Saturday",
  period : "morning",
  temperature : 27,
+ city : "trento",
  condition : "cloudy"
 },   
 {
  day : "Saturday",
  period : "noon",
  temperature : 30,
+ city : "trento",
  condition : "cloudy"
 },   
 {
  day : "Saturday",
  period : "afternoon",
  temperature : 31,
+ city : "trento",
  condition : "cloudy"
 },   
 {
  day : "Saturday",
  period : "evening",
  temperature : 29,
+ city : "trento",
  condition : "cloudy"
-}
+},
+{
+ day : "Saturday",
+ period : "morning",
+ temperature : 27,
+ city : "rovereto",
+ condition : "cloudy"
+},   
+{
+ day : "Saturday",
+ period : "noon",
+ temperature : 32,
+ city : "rovereto",
+ condition : "cloudy"
+},   
+{
+ day : "Saturday",
+ period : "afternoon",
+ temperature : 33,
+ city : "rovereto",
+ condition : "cloudy"
+},   
+{
+ day : "Saturday",
+ period : "evening",
+ temperature : 30,
+ city : "rovereto",
+ condition : "cloudy"
+},   
   
 ];
