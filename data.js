16d8b6d6a56ca247b54b15b6bf7989e6cf897f66
Marcos Baez <mbaezpy@gmail.com>
2015-11-24 14:18:47 +0100

    Initial version of Calor

---
+++ data.js
@@ -0,0 +1,103 @@
+/* Don't modify this file */
+
+var data = [
+{
+ day : "Wednesday",
+ period : "morning",
+ temperature : 29,
+ condition: "sunny"
+},   
+{
+ day : "Wednesday",
+ period : "noon",
+ temperature : 32,
+ condition: "sunny"
+},   
+{
+ day : "Wednesday",
+ period : "afternoon",
+ temperature : 34,
+ condition: "sunny"
+},   
+{
+ day : "Wednesday",
+ period : "evening",
+ temperature : 33,
+ condition: "sunny"
+},
+{
+ day : "Thursday",
+ period : "morning",
+ temperature : 27,
+ condition: "sunny-cloudy"
+},   
+{
+ day : "Thursday",
+ period : "noon",
+ temperature : 31,
+ condition: "sunny-cloudy"
+},   
+{
+ day : "Thursday",
+ period : "afternoon",
+ temperature : 32,
+ condition: "sunny-cloudy"
+},   
+{
+ day : "Thursday",
+ period : "evening",
+ temperature : 30,
+ condition: "sunny-cloudy"
+},
+ 
+{
+ day : "Friday",
+ period : "morning",
+ temperature : 28,
+ condition : "sunny-rainy"
+},   
+{
+ day : "Friday",
+ period : "noon",
+ temperature : 32,
+ condition : "sunny-rainy"
+},   
+{
+ day : "Friday",
+ period : "afternoon",
+ temperature : 33,
+ condition : "sunny-rainy"
+},   
+{
+ day : "Friday",
+ period : "evening",
+ temperature : 31,
+ condition : "sunny-rainy"
+},
+
+{
+ day : "Saturday",
+ period : "morning",
+ temperature : 27,
+ condition : "cloudy"
+},   
+{
+ day : "Saturday",
+ period : "noon",
+ temperature : 30,
+ condition : "cloudy"
+},   
+{
+ day : "Saturday",
+ period : "afternoon",
+ temperature : 31,
+ condition : "cloudy"
+},   
+{
+ day : "Saturday",
+ period : "evening",
+ temperature : 29,
+ condition : "cloudy"
+}
+  
+];
